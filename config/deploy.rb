7fa6a588d1f2491a5001c1fe62243aaff8ea8ce5
masayoshizz <masayoshizz@gmail.com>
2017-10-20 08:50:57 +0000

    fix config/deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -46,7 +46,7 @@ set :rbenv_type, :user
 set :rbenv_ruby, '2.3.1'
 
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['Users/tech-camp/.ssh/keypairAWS.pem']
+                  keys: ['/home/ubuntu/.ssh/keypairAWS.pem']
 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
