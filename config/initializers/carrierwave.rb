e5576075d498341ad5c3f954f2db0de6d9cb5407
masayoshizz <masayoshizz@gmail.com>
2017-10-20 12:27:14 +0000

    carrierwave chaged

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,8 +7,8 @@ CarrierWave.configure do |config|
   config.fog_credentials = {
     provider: 'AWS',
     aws_access_key_id: ENV['aws_access_key_id'],
-    aws_secret_access: ENV['aws_secret_access_key'],
-    region: 'us-east-1'
+    aws_secret_access_key: ENV['aws_secret_access_key'],
+    region: 'ap-northeast-1'
   }
 
   config.fog_directory  = 'ma-yo-mi-bucket'
